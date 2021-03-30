7fc408347437bc3e753904301b77f2e8048a7334
Swastik Mukherjee <swastikrik@gmail.com>
2021-03-30 16:18:18 +0530

    fixing security issuses updated libraries

---
+++ src/containers/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import SearchBox from '../componets/SearchBox';
+import CardList from '../componets/CardList';
+import './App.css';
+import Scroll from'../componets/Scroll';
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+    componentDidMount() {
+          fetch('https://jsonplaceholder.typicode.com/users')
+          .then(Response => Response.json())
+          .then(users => this.setState({robots:users}));
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value })
+    }
+    render () {
+        const {robots,searchfield  } =this.state
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+        return !robots.length  ?
+         <h1>Loading</h1> :
+            (
+              <div className = 'tc'>
+                  <h1 className = 'f1'>RoboFriends</h1>
+                  <SearchBox searchChange ={this.onSearchChange}/>
+                  <Scroll>
+                       <CardList robots = {filteredRobots}/>
+                  </Scroll>
+                </div>
+            );
+    }
+}
+export default App;
